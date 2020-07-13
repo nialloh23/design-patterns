6fb4e989ce82c73fd8ed09c8d3a9b58d150dc0e8
Niall O Hara <niall@changex.org>
2020-07-13 17:23:34 +0100

    revised factory & builder patterns. played_around and made notion notes

---
+++ factory_pattern/abstract_factory_2.py
@@ -0,0 +1,76 @@
+from abc import ABC
+from enum import Enum, auto
+
+
+class HotDrink(ABC):
+    def consume(self):
+        pass
+
+class Tea(HotDrink):
+    def consume(self):
+        print('This tea is nice but I\'d prefer it with milk')
+
+class Coffee(HotDrink):
+    def consume(self):
+        print('This coffee is delicious')
+
+
+class HotDrinkFactory(ABC):
+    def prepare(self, amount):
+        pass
+
+
+class TeaFactory(HotDrinkFactory):
+    def prepare(self, amount):
+        print(f'Put in tea bag, boil water, pour {amount}ml, enjoy!')
+        return Tea()
+
+
+class CoffeeFactory(HotDrinkFactory):
+    def prepare(self, amount):
+        print(f'Grind some beans, boil water, pour {amount}ml, enjoy!')
+        return Coffee()
+
+
+
+class HotDrinkMachine:
+    class AvailableDrink(Enum):
+        COFFEE = auto()
+        TEA = auto()
+    factories = []
+    initialized = False
+
+    def __init__(self):
+        if not self.initialized:
+            self.initialized = True
+            for d in self.AvailableDrink:
+                name = d.name[0] + d.name[1:].lower()
+                factory_name = name + 'Factory'
+                factory_instance = eval(factory_name)()
+                self.factories.append((name, factory_instance))
+
+
+    def make_drink(self):
+        print('Available Drinks:')
+        for f in self.factories:
+            print(f[0])
+
+        s = input(f'Please pick drink (0-{len(self.factories)-1}): ')
+        idx = int(s)
+        s = input(f'Specify amount: ')
+        amount = int(s)
+
+        return self.factories[idx][1].prepare(amount)
+
+
+#### 1. Tell you which drinks are available (1 or 2)
+#### 2. Pick a drink
+#### 3. Specify how many ML you want?
+
+
+if __name__ == '__main__':
+    factory = CoffeeFactory()
+    factory.prepare(10)
+    machine = HotDrinkMachine()
+    print(machine.factories)
+    machine.make_drink()
